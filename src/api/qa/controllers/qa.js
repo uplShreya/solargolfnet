e9ae6123dd32f8a4ad63f67fb1b3cce565194884
uplShreya <shreya.saha@underscoretec.com>
2023-08-21 15:58:40 +0530

    structure created

---
+++ src/api/qa/controllers/qa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * qa controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::qa.qa');
---
+++ src/api/qa/routes/qa.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * qa router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::qa.qa');
